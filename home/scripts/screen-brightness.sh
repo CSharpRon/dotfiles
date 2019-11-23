2d3093a0fcdf72d5f3beab1fbbd7eeb8b8d97fbe
CSharpRon <ronald.marrero@outlook.com>
2019-11-22 20:32:12 -0500

    Adding .bash folder

--- home/scripts/screen-brightness.sh
+++ home/scripts/screen-brightness.sh
@@ -3,6 +3,9 @@
 switch=$1
 if [ $switch == "+" ]; then
     xbacklight -inc 5
+    if [ $(xbacklight) = 100.000000 ]; then
+        notify-send --icon=gtk-info "Maximum screen brightness reached"
+    fi
 else
     xbacklight -dec 5
 fi
